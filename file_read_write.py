ccabf991c00d7275b740e94b9ad0d8416a20c2f7
Денис <giliaz.d@gmail.com>
2022-06-22 21:55:56 +0300

    first commit

---
+++ file_read_write.py
@@ -0,0 +1,11 @@
+with open("dataset_24465_4.txt") as test,  open('test_copy_1.txt', 'w') as test_copy:
+    #new = []
+    #for line in test:
+    #    new.append(line.rstrip())
+    #for i in range(len(new) - 1, -1, -1):
+    #    test_copy.write(new[i]+'\n')
+
+    #for lines in reversed(test.readlines()):
+    #    test_copy.write(lines)
+
+    test_copy.writelines(reversed(test.readlines()))
