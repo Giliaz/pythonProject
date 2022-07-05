5406103a43b6c5a9bd4e2503278265a0f5862cc4
Денис <giliaz.d@gmail.com>
2022-07-06 01:00:23 +0300

    add old functional and make changes

--- home_flask_1.py
+++ home_flask_1.py
@@ -1,6 +1,6 @@
-import re
 import random
 import csv
+import string
 
 from flask import Flask
 
@@ -14,38 +14,54 @@ def index():
 #passwoerd
 @app.route('/password')
 def generate_password():
-    password = ([chr(random.randrange(33, 127)) for _ in range(random.randrange(10, 21))])
-    pas_check = ''.join(password)
-    return f"<center><h2><u>Password lenght {len(password)} symb.</u>:  {pas_check}</h2></center>"
+     password = ([chr(random.randrange(33, 127)) for _ in range(random.randrange(10, 21))])
+     pas_check = ''.join(password)
+     return f"<center><h2><u>Password lenght {len(password)} symb.</u>:  {pas_check}</h2></center>"
 
-#  єто я так задолбался в первій раз)))
-# password = []
-# for _ in range(random.randrange(2, 5)):
-#     password.append((string.ascii_uppercase)[random.randrange(len(string.ascii_uppercase))])
-#     password.append((string.ascii_lowercase)[random.randrange(len(string.ascii_lowercase))])
-# for _ in range(random.randrange(3, 5)):
-#     password.append((string.digits)[random.randrange(len(string.digits))])
-#     password.append((string.punctuation)[random.randrange(len(string.punctuation))])
-# random.shuffle(password)
-# pas = ''.join(password)
-# return f"<center><h2><u>Password lenght {len(password)} symb.</u>:  {(''.join(password))}</h2></center>"
+# еще один вариант
+    #num = random.randrange(10, 21)
+    #password = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation, k=num))
+    #return f"<center><h2><u>Password lenght {len(password)} symb.</u>:  {password}</h2></center>"
+
+# Это я так задолбался в первій раз)))
+#   password = []
+#   for _ in range(random.randrange(2, 5)):
+#       password.append((string.ascii_uppercase)[random.randrange(len(string.ascii_uppercase))])
+#       password.append((string.ascii_lowercase)[random.randrange(len(string.ascii_lowercase))])
+#   for _ in range(random.randrange(3, 5)):
+#       password.append((string.digits)[random.randrange(len(string.digits))])
+#       password.append((string.punctuation)[random.randrange(len(string.punctuation))])
+#   random.shuffle(password)
+#   pas = ''.join(password)
+#   return f"<center><h2><u>Password lenght {len(password)} symb.</u>:  {(''.join(password))}</h2></center>"
 
 #average
 @app.route('/average')
 def get_average_parameters():
-    # откріваем файл создаем список из строк файла CSV
-    with open("hw.csv", newline='') as file:
-        reader = csv.DictReader(file, delimiter=':')
-        data = [(row['Index, Height(Inches), Weight(Pounds)']).split(',') for row in reader]
-    height = weight = 0.0
-    # перебираем список и суммируем данные
-    for row in data:
-        height += float(row[1])
-        weight += float(row[2])
-    # считаем среднее и округляем до двух знаков после запятой
-    average_height = round(height/len(data), 2)
-    average_weight = round(weight/len(data), 2)
-    return f"<center><h2><u>average_height</u> = {average_height} inches, <u>average_weight</u> = {average_weight} pounds.</h2></center>"
+# первый вариант
+   with open("hw.csv", newline='') as file:
+       reader = csv.DictReader(file, delimiter=':')
+       data = [(row['Index, Height(Inches), Weight(Pounds)']).split(',') for row in reader]
+   height = weight = 0.0
+   for row in data:
+       height += float(row[1])
+       weight += float(row[2])
+   average_height = round(height/len(data), 2)
+   average_weight = round(weight/len(data), 2)
+   return f"<center><h2><u>average_height</u> = {average_height} inches, <u>average_weight</u> = {average_weight} pounds.</h2></center>"
+
+# еще один вариант CSV
+# with open("hw.csv", newline='') as file:
+#     reader = csv.DictReader(file, delimiter=',')
+#     count = 0
+#     height = weight = 0.0
+#     for row in reader:
+#         height += float(row[' Height(Inches)'])
+#         weight += float(row[' Weight(Pounds)'])
+#         count += 1
+#     average_height: float = round((height/count), 2)
+#     average_weight = round((weight/count), 2)
+#     return f"<center><h2><u>average_height</u> = {average_height} inches, <u>average_weight</u> = {average_weight} pounds.</h2></center>"
 
 if __name__ == '__main__':
     app.run(debug=True)
