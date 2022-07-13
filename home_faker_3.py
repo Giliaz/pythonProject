56a9e2d10003b448196a99008af9228ce1cd9b36
Денис <giliaz.d@gmail.com>
2022-07-14 02:09:40 +0300

    first commit create one path

---
+++ home_faker_3.py
@@ -0,0 +1,39 @@
+import csv
+
+from flask import Flask, request, jsonify, Response
+from webargs import validate, fields
+from webargs.flaskparser import use_kwargs
+
+from faker import Faker
+faker_instance = Faker("EN")
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Start page....</p>"
+
+@app.route('/csv')
+@use_kwargs({"row": fields.Int(missing=10, validate=[validate.Range(min=1, max=1000)]), }, location="query")
+def generate_password(row):
+    with open("person.csv", 'w', encoding='utf8', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=";")
+        writer.writerow(['first_name', 'last_name', 'email', 'password', 'date_of_birth'])
+        for _ in range(row):
+            person = []
+            person.append(faker_instance.first_name())
+            person.append(faker_instance.last_name())
+            person.append(faker_instance.email())
+            person.append(faker_instance.password())
+            person.append(faker_instance.date_of_birth(minimum_age=20, maximum_age=70).strftime("%m.%d.%Y"))
+            writer.writerow(person)
+    with open("person.csv", encoding='utf8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        line = ''
+        for row in reader:
+            line += f'{(str(row))}<br>'
+    return (line)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
