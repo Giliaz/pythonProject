7dc86198badfe7784bad4717828ce03b88d91128
Денис <giliaz.d@gmail.com>
2022-07-20 15:08:49 +0300

    End version of hw4

--- hw_4/sql_flask_hw4.py
+++ hw_4/sql_flask_hw4.py
@@ -20,8 +20,8 @@ def index():
 @use_kwargs({"country": fields.Str(load_default="%"), }, location="query")
 def get_country_sales(country):
     query = f"SELECT UnitPrice AS Price_Unit, count(Quantity) AS Total_Quanity,(count(Quantity) * UnitPrice) AS Total_Sales " \
-            f"FROM invoice_items " \
-            f"JOIN invoices i on invoice_items.InvoiceId = i.InvoiceId " \
+            f"FROM invoice_items as i1 " \
+            f"JOIN invoices as i2 on i1.InvoiceId = i2.InvoiceId " \
             f"WHERE BillingCountry LIKE '{country}';"
 
     records = execute_query(query)
@@ -33,32 +33,29 @@ def get_country_sales(country):
 @app.route('/track_id')
 @use_kwargs({"id": fields.Int(missing=1, validate=[validate.Range(min=1, max=3503)]), }, location="query")
 def get_track_id(id):
-    query = f"SELECT tracks.Name AS Track, a3.Name AS Team, tracks.Composer AS Composers," \
-                    f" a2.Title AS Album, g.Name AS Genre, mt.Name AS Media, (tracks.Milliseconds/60000) AS 'Longest(min.)', " \
-                    f" (tracks.Bytes/1000000) AS 'Size(MB)', tracks.UnitPrice AS 'Price(USD)' " \
-            f"FROM tracks " \
-            f"JOIN playlist_track pt on tracks.TrackId = pt.TrackId, " \
-                    f" playlists p join playlist_track t on p.PlaylistId = t.PlaylistId, " \
-                    f" tracks t2 join genres g on g.GenreId = t2.GenreId, " \
-                    f" tracks t3 join albums a on a.AlbumId = t3.AlbumId, " \
-                    f" albums a2 join artists a3 on a3.ArtistId = a2.ArtistId, " \
-                    f" albums a4 join media_types mt on mt.MediaTypeId = t3.MediaTypeId, " \
-                    f" tracks t4 join invoice_items ii on t.TrackId = ii.TrackId, " \
-                    f" invoice_items i join invoices i2 on i2.InvoiceId = i.InvoiceId, " \
-                    f" invoices i3 join customers c on i3.CustomerId = c.CustomerId, " \
-                    f" customers c2 join employees e on c2.SupportRepId = e.EmployeeId " \
-            f"WHERE tracks.TrackId = {id} " \
-            f"LIMIT 1;"
+    query = f"SELECT t.TrackId, t.Name AS Track, a.Title AS Album, a2.Name AS Team, t.Composer AS Composers, " \
+            f"g.Name AS Genre, GROUP_CONCAT(pl.Name) AS Play_Lists, mt.Name AS Media_Type, " \
+            f"t.Milliseconds/60000 AS Track_Time, t.Bytes/1000000 AS Track_Size, t.UnitPrice AS Price " \
+            f"FROM tracks as t " \
+            f"JOIN albums as a on t.AlbumId = a.AlbumId " \
+            f"JOIN artists as a2 on a.ArtistId = a2.ArtistId " \
+            f"JOIN genres as g on t.GenreId = g.GenreId " \
+            f"JOIN media_types as mt on t.MediaTypeId = mt.MediaTypeId " \
+            f"JOIN playlist_track as plt on t.TrackId = plt.TrackId " \
+            f"JOIN playlists as pl on plt.PlaylistId = pl.PlaylistId " \
+            f"WHERE t.TrackId LIKE {id};"
 
-    records = execute_query(query)
+    records = [('TrackId', 'Track', 'Album', 'Team', 'Composers', 'Genre',
+                     'Play_Lists', 'Media_Type', 'Time(min)','Size(mb)', 'Price')]
+    records.extend(execute_query(query))
     return render_template("table_from_list.html", csv=records)
 
 
 @app.route('/time_all_tracks')
 def get_time():
-    query = f"SELECT SUM(Milliseconds)/3600000" \
-            f" FROM tracks" \
-            f" JOIN albums a on tracks.AlbumId = a.AlbumId;"
+    query = f"SELECT SUM(Milliseconds)/3600000 " \
+            f"FROM tracks as t " \
+            f"JOIN albums as a on t.AlbumId = a.AlbumId;"
 
     return f'<h3>Total time all tracks: {execute_query(query)[0][0]} hrs.<h/3>'
 
