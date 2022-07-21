86aa952866045fe070cd06114c271713042ac8ce
Денис <giliaz.d@gmail.com>
2022-07-21 19:10:17 +0300

    new options

--- hw_5/sql_hw5.py
+++ hw_5/sql_hw5.py
@@ -1,9 +1,8 @@
-from webargs import validate, fields
+from flask import Flask, render_template
+from webargs import fields
 from webargs.flaskparser import use_kwargs
 
-from flask import Flask, render_template
-
-from sql_handler import execute_query
+from hw_5.sql_handler import execute_query
 
 app = Flask(__name__)
 
@@ -11,23 +10,30 @@ app = Flask(__name__)
 # choice page
 @app.route('/')
 def index():
-    return '<h3>Start page (tap choice): <a href = "country_sales">country_sales</a> (?country=..), ' \
-           '<a href = "track_id"> track_id</a> (?id=..), <a href = "time_all_tracks"> time_all_tracks</a>.</h3>'
+    return '<h3>Start page (tap choice): <a href = "genres_sales">genres_sales</a> (?genre=..) </h3>'
 
 
 # country sales
-@app.route('/country_sales')
-@use_kwargs({"country": fields.Str(load_default="%"), }, location="query")
-def get_country_sales(country):
-       query = f"SELECT BillingCity AS City, SUM(Quantity) AS Total_Tracks " \
-              f"FROM invoices as i " \
-              f"INNER JOIN invoice_items as i2 on i.InvoiceId = i2.InvoiceId " \
-              f"INNER JOIN tracks as t on i2.TrackId = t.TrackId " \
-              f"INNER JOIN genres as g on t.GenreId = g.GenreId " \
-              f"WHERE g.Name is 'Metal' " \
-              f"GROUP BY BillingCity " \
-              f"ORDER BY SUM(Quantity) DESC " \
-              f"LIMIT 1;
+@app.route('/genres_sales')
+@use_kwargs({"genre": fields.Str(load_default=''), }, location="query")
+def get_country_sales(genre):
+    query = f"SELECT g.Name, BillingCity AS City, SUM(Quantity) AS Total_Tracks " \
+            f"FROM invoices as i " \
+            f"INNER JOIN invoice_items as i2 on i.InvoiceId = i2.InvoiceId " \
+            f"INNER JOIN tracks as t on i2.TrackId = t.TrackId " \
+            f"INNER JOIN genres as g on t.GenreId = g.GenreId " \
+            f"WHERE g.Name LIKE '{genre}' " \
+            f"GROUP BY BillingCity " \
+            f"ORDER BY SUM(Quantity) DESC " \
+            f"LIMIT 1; "
+
+    records = [('Genre music', 'City of best sales', 'Sales(tracks)')]
+    records.extend(execute_query(query))
+
+    if (len(records) < 2) & (genre != ''):
+        return f'<h3>genres_sales(?genre=..)<br><font color="red">Genre is not correct or not find in database...</color>{render_template("table_from_list.html", csv=records)}</h3>'
+    else:
+        return f'<h3>genres_sales(?genre=..)</h3>{render_template("table_from_list.html", csv=records)}'
 
 
 
