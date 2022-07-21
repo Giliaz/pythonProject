486744dcf9bbf91289209a26281090bf2b848bee
Денис <giliaz.d@gmail.com>
2022-07-21 15:10:43 +0300

    new options

--- hw_5/point_hw5.py
+++ hw_5/point_hw5.py
@@ -1,15 +1,17 @@
 class Circle:
-    def __init__(self, x, y, radius):
+    def __init__(self, x=0, y=0, radius=10):
         self.x = x
         self.y = y
         self.radius = radius
+        print(self.x, self.y, self.radius)
 
     def in_circle(self, f):
+        print(f.x,  f.y)
         return (self.x - f.get_x()) ** 2 + (self.y - f.get_y()) ** 2 < self.radius ** 2
 
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
@@ -19,8 +21,9 @@ class Point:
     def get_y(self):
         return self.y
 
-#start x, y, radius
-a = Circle(0, 10, 15)
-#point for find
-b = Point(5, 5)
+#circle x, y, radius
+a = Circle(x=5, y=5, radius=15)
+#point x, y
+b = Point(x=-5, y=-5)
+
 print(a.in_circle(b))
