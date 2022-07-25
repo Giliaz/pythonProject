ad125dda111761587d7c3643199206db87859983
Денис <giliaz.d@gmail.com>
2022-07-25 20:14:58 +0300

    class figures

--- hw_6/figures.py
+++ hw_6/figures.py
@@ -19,6 +19,15 @@ class Circle(Shape):
     def square(self):
         return math.pi * self.radius ** 2
 
+    def __in__(self, point):
+        return (self.x - point.x) ** 2 + (self.y - point.y) ** 2 < self.radius ** 2
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
 
 class Rectangle(Shape):
 
