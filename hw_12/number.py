17c2f920c718b0c8a886e180305582cf56658b53
Денис <giliaz.d@gmail.com>
2022-08-10 23:50:01 +0300

    first commit

--- hw_12/number.py
+++ hw_12/number.py
@@ -1,6 +1,15 @@
-num = int(input('Enter number: '))
-result = []
-while num > 0:
-    result.append(str(num % 1000))
-    num //= 1000
-print(f"Your result: {'.'.join(result[::-1])}")
+def new_format(string):
+    result = []
+    while len(string) > 0:
+        result.append(string[-3:])
+        string = string[:-3]
+
+    return '.'.join(result[::-1])
+
+
+assert (new_format("1000000") == "1.000.000")
+assert (new_format("100") == "100")
+assert (new_format("1000") == "1.000")
+assert (new_format("100000") == "100.000")
+assert (new_format("10000") == "10.000")
+assert (new_format("0") == "0")
