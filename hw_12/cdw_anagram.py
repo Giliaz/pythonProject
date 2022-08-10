3322473ef2937c86184010227e1309ffd99f40f0
Денис <giliaz.d@gmail.com>
2022-08-11 02:06:32 +0300

    anagram run

---
+++ hw_12/cdw_anagram.py
@@ -0,0 +1,7 @@
+def cdw_anagrams(word, words):
+    return [ann_ for ann_ in words if sorted(word) == sorted(ann_)]
+
+
+
+print(cdw_anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
+
--- hw_12/cdw_narciss.py
+++ hw_12/cdw_narciss.py
@@ -1,10 +1,10 @@
 def cdw_narciss_num(number):
-    degree = len(str(number))
     num = number
     summ = 0
     while num > 0:
-        summ += pow(num % 10, degree)
+        summ += pow(num % 10, len(str(number)))
         num //= 10
+
     return number == summ
 
 
