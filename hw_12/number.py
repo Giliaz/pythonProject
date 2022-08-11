c77dd5d6338c403ac3c1a5f588100fcda9034e80
Денис <giliaz.d@gmail.com>
2022-08-11 23:57:26 +0300

    number.py last version

--- hw_12/number.py
+++ hw_12/number.py
@@ -1,15 +1,27 @@
+# def new_format(string):
+#     result = []
+#     while len(string) > 0:
+#         result.append(string[-3:])
+#         string = string[:-3]
+#
+#     return '.'.join(result[::-1])
+#
+
 def new_format(string):
-    result = []
-    while len(string) > 0:
-        result.append(string[-3:])
-        string = string[:-3]
+    result = ''
+    count = 0
+    for i in string[::-1]:
+        if count % 3 == 0 and count != 0:
+            result += '.'
+        count += 1
+        result += i
 
-    return '.'.join(result[::-1])
+    return result[::-1]
 
 
-assert (new_format("1000000") == "1.000.000")
-assert (new_format("100") == "100")
-assert (new_format("1000") == "1.000")
-assert (new_format("100000") == "100.000")
-assert (new_format("10000") == "10.000")
-assert (new_format("0") == "0")
+# assert (new_format("1000000") == "1.000.000")
+# assert (new_format("100") == "100")
+# assert (new_format("1000") == "1.000")
+# assert (new_format("100000") == "100.000")
+# assert (new_format("10000") == "10.000")
+# assert (new_format("0") == "0")
